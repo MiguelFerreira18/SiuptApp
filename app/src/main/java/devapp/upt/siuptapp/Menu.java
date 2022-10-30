b0386d785392f6766c582dd4322c7809a486a26e
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-10-30 17:48:54 +0000

    Horario do estudante com ligação à api feito

---
+++ app/src/main/java/devapp/upt/siuptapp/Menu.java
@@ -0,0 +1,21 @@
+package devapp.upt.siuptapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+public static String tokenS = "token";
+Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        i = getIntent();
+        //PRECISO DO TOKEN DO LOGIN
+    }
+
+
+
+}
