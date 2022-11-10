3257dbb3db75035b273a124ec4b430b80ba5b818
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-11-10 16:05:50 +0000

    Time table com cores

--- app/src/main/java/devapp/upt/siuptapp/Menu.java
+++ app/src/main/java/devapp/upt/siuptapp/Menu.java
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
-public static final String tokenS = "token";
-Intent i;
+    public static final String tokenS = "token";
+    Intent i;
+    String token;
+    Button bt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         i = getIntent();
-        //PRECISO DO TOKEN DO LOGIN
+        token = i.getStringExtra(MainActivity.tokenA);
+        bt1 = findViewById(R.id.myBtn);
+        bt1.setOnClickListener(this::onClick);
     }
-
+    public void onClick(View v){
+        Intent i = new Intent(this, TimeTable.class);
+        i.putExtra(tokenS, token);
+        startActivity(i);
+    }
 
 
 }
