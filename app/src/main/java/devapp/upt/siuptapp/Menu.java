8b948b1e8b76bc24d09e817e6b2811e04a2530c2
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-11-17 18:19:06 +0000

    Ementa corrigida e feita

--- app/src/main/java/devapp/upt/siuptapp/Menu.java
+++ app/src/main/java/devapp/upt/siuptapp/Menu.java
@@ -14,19 +14,26 @@ public class Menu extends AppCompatActivity {
     public static final String tokenS = "token";
     String token;
     Intent i;
-    ImageView timeTablechange, notasChange;
+    ImageView timeTablechange, notasChange,ementaChange;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //INTENT11
         i = getIntent();
         token = i.getStringExtra(MainActivity.tokenA);
+
+        //IDS
         notasChange = findViewById(R.id.NotasMenu);
         timeTablechange = findViewById(R.id.TimeTableMenu);
+        ementaChange = findViewById(R.id.Ementa);
+
+        //ONCLICK
         notasChange.setOnClickListener(this::onClick);
         timeTablechange.setOnClickListener(this::onClick);
+        ementaChange.setOnClickListener(this::onClick);
     }
 
     public void onClick(View v) {
@@ -40,6 +47,10 @@ public class Menu extends AppCompatActivity {
                 intent = new Intent(this, TimeTable.class);
                 Log.d("Cenas", "onClick: " + i.getStringExtra(MainActivity.tokenA));
                 intent.putExtra(tokenS, token);
+                startActivity(intent);
+                break;
+            case R.id.Ementa:
+                intent = new Intent(this, Ementa.class);
                 startActivity(intent);
                 break;
         }
